e34acbbc98275a87e2ba83f6ec2027eedc610b51
ramazanguzel <crsrare@gmail.com>
2024-07-30 23:28:25 +0300

    small fix

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -3,12 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 
 import User from "@models/user";
 import { connectToDB } from "@utils/database";
-/*
-console.log({
-  clientId: process.env.GOOGLE_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-});
-*/
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
