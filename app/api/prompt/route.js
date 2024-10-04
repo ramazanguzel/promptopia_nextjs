9f854e3864cdd28674526235258caae512941d8d
ramazanguzel <crsrare@gmail.com>
2024-10-04 04:33:44 +0300

    mongodb fix

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -4,7 +4,7 @@ import Prompt from "@models/prompt";
 export const GET = async (request) => {
   try {
     await connectToDB();
-
+    // fix error related to db
     const prompts = await Prompt.find({}).populate("creator");
 
     return new Response(JSON.stringify(prompts), { status: 200 });
